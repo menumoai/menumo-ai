16d91db774fee6741012a260ba1245a6ddb09899
AntoineSfeir <antoinegsfeir@gmail.com>
2025-12-03 20:25:34 -0600

    fixed build issues

--- src/pages/OrdersPage.tsx
+++ src/pages/OrdersPage.tsx
@@ -1,4 +1,5 @@
-import { useEffect, useMemo, useState, FormEvent } from "react";
+import { useEffect, useMemo, useState } from "react";
+import type { FormEvent } from "react";
 import { Link } from "react-router-dom";
 
 import { listOrders, createOrderWithLineItems } from "../services/order";
